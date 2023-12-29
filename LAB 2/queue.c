27498da73776f2b4d040b5c0a26ba75c3ea6c879
nithinkoushik <153620277+nithinkoushik@users.noreply.github.com>
2023-12-29 10:24:50 +0530

    Update queue.c

--- LAB 2/queue.c
+++ LAB 2/queue.c
@@ -1,31 +1,59 @@
 #include<stdio.h>
-#define max_size 100
-int   queue[max_size];
-int front=-1;int rear=-1;
-void enqueue(int);
+#define max 20
+int queue[max];
+int front=-1, rear=-1;
+
+void enqueue(int a);
 int dequeue();
 void display();
-void main()
-{
-  int choice;
-printf("1.TO INSERT ENQUEUE\n2.TO DEQUEUE\n3.TO DISPLAY");
-scanf("%d",&choice);
-switch(choice)
-{
-case 1:int ele;
-       printf("enter the element to insert\n");
-      scanf("%d",&ele);
-      void enqueue(ele); 
-break;
-case 2: int rem=dequeue();
-return rem;
-break;
-case 3:void display();
-break;
-default:printf("enter correct input");
-}
-void enqueue(int m)
-{
-if(rear==(max_size  
 
+void main(){
+    int n,m;
+    printf("Enter\n1.To insert into queue\n2.To remove from queue\n3.To display queue:\n");
+    scanf("%d",&n);
+    while(n!=3){
+        switch(n){
+            case 1: printf("Enter element to enter into queue:");
+                    scanf("%d",&m);
+                    enqueue(m);
+                    break;
+                    
+            case 2: m=dequeue();
+                    printf("Removed element is %d\n",m);
+                    break;
+        
+            default: printf("Invalid input");
+        }
+        scanf("%d",&n);
+        printf("\n");
+    }
+    if(n==3){
+            display();
+        }
+}
+
+void enqueue(int a){
+    if(rear==max-1){
+        printf("Queue overflow");
+    }
+    rear+=1;
+    queue[rear]=a;
+}
+
+int dequeue(){
+    if(rear==-1 || front==rear){
+        printf("Queue underflow");
+    }
+    front+=1;
+    return queue[front];
+}
+
+void display(){
+    
+    printf("Queue:\n");
+    for(int i=front+1;i<=rear;i++){
+        printf("%d\t",queue[i]);
+        }
+    
+}
   
